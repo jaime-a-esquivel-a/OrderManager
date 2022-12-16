3772db515eb6f0bb3d3adae563d835fca6311a5a
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2022-12-16 14:24:20 -0600

    Adding files for the server, the connection and the routes for the application

---
+++ controllers/clients-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+
+//Ruta para traer todos los clientes
+router.get('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
+
+//Ruta para traer un cliente
+router.get('/:id', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
+
+//Ruta para crear un nuevo cliente
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+//Ruta para actualizar/modificar un cliente
+router.put('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
+
+//Ruta para eliminar un cliente
+router.delete('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
+
+module.exports = router;
---
+++ controllers/orders-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+
+//Ruta para traer todas las órdenes
+router.get('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
+
+//Ruta para traer una orden
+router.get('/:id', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
+
+
+//Ruta para crear una nueva orden
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+});
+
+//Ruta para actualizar/modificar una orden
+router.put('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
+
+//Ruta para eliminar una orden
+router.delete('/', async (req, res) => {
+    try {
+
+    } catch (error) {
+        
+    }
+});
+
+module.exports = router;
