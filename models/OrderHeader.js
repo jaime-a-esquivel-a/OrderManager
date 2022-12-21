53b90ec98cfbb51960494669c4caaaf3550c9b35
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2022-12-20 18:05:27 -0600

    Modelo OrderHeader.js creado

---
+++ models/OrderHeader.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class OrderHeader extends Model {}
+
+OrderHeader.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        },
+        client_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'client',
+                key: 'id',
+            }
+        },
+        status_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'orderstatus',
+                key: 'id',
+            }
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'orderstatus',
+    }
+);
+
+module.exports = OrderHeader;
