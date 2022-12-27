8780c2a6d8829f3971b8572b345190c5dda0b903
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2022-12-27 12:32:02 -0600

    Client routes created, return statement added for users & materials

--- controllers/clients-routes.js
+++ controllers/clients-routes.js
@@ -1,20 +1,65 @@
 const router = require('express').Router();
+const Client = require ('../models/Client');
 
 //Ruta para traer todos los clientes
 router.get('/', async (req, res) => {
     try {
 
+        const clientsData = await Client.findAll({});
+
+        if(!clientsData){
+            res.status(404).json({message: "No clients found in database"});
+            return;
+        }
+
+        res.status(200).json(clientsData);
+
     } catch (error) {
-        
+        res.status(500).json(error);
     }
 });
 
-//Ruta para traer un cliente
-router.get('/:id', async (req, res) => {
+//Ruta para traer un cliente por email
+router.get('/email/:email', async (req, res) => {
     try {
 
+        const clientData = await Client.findOne({
+            where:{
+                email: req.params.email,
+            }
+        });
+
+        if(!clientData){
+            res.status(404).json({message: "No client was found with that email in database"});
+            return;
+        }
+
+        res.status(200).json(clientData);
+
     } catch (error) {
-        
+        res.status(500).json(error);
+    }
+});
+
+//Ruta para traer un cliente por RFC
+router.get('/rfc/:rfc', async (req, res) => {
+    try {
+
+        const clientData = await Client.findOne({
+            where:{
+                rfc: req.params.rfc,
+            }
+        });
+
+        if(!clientData){
+            res.status(404).json({message: "No client was found with that rfc in database"});
+            return;
+        }
+
+        res.status(200).json(clientData);
+
+    } catch (error) {
+        res.status(500).json(error);
     }
 });
 
@@ -22,26 +67,63 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
 
-    } catch (error) {
+        const newClient = await Client.create({
+            rfc: req.body.rfc,
+            active: req.body.active,
+            name: req.body.name,
+            address: req.body.address,
+            tel: req.body.tel,
+            email: req.body.email,
+        });
 
+        res.status(200).json(newClient);
+
+    } catch (error) {
+        res.status(500).json(error);
     }
 });
 
 //Ruta para actualizar/modificar un cliente
-router.put('/', async (req, res) => {
+router.put('/:rfc', async (req, res) => {
     try {
 
+        const updateClient = await Client.update(req.body, {
+            where: {
+                rfc: req.params.rfc,
+            },
+        });
+
+        if(!updateClient[0]){
+            res.status(404).json({message: "No client was found with that rfc in database"});
+            return;
+        }
+
+        res.status(200).json(updateClient);
+
     } catch (error) {
-        
+        res.status(500).json(error);
     }
 });
 
 //Ruta para eliminar un cliente
-router.delete('/', async (req, res) => {
+router.delete('/:rfc', async (req, res) => {
     try {
 
+        const deleteClient = await Client.destroy({
+            where: {
+                rfc: req.params.rfc,
+            },
+        });
+
+        if(!deleteClient){
+            res.status(404).json({message: "No client was found with that rfc in database"});
+            return;
+        }
+
+        res.status(200).json(deleteClient);
+
     } catch (error) {
-        
+        res.status(500).json(error);
     }
 });
 
